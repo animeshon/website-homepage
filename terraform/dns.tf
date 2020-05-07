06358743bf63fc234cc744266ed01667923a9fd9
Christian Roggia <christian.roggia@gmail.com>
2020-05-07 20:05:15 +0200

    Use A and AAAA records

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -5,12 +5,27 @@ locals {
 }
 
 # Setup homepage preview subdomain managed by Google Cloud Run.
+locals {
+  dns_records = {
+    "A" = [
+      for rr in google_cloud_run_domain_mapping.homepage.resource_records :
+      rr.rrdata if rr.type == "A"
+    ]
+    "AAAA" = [
+      for rr in google_cloud_run_domain_mapping.homepage.resource_records :
+      rr.rrdata if rr.type == "AAAA"
+    ]
+  }
+}
+
 resource "google_dns_record_set" "homepage" {
+  for_each = local.dns_records
+
   project      = local.zone_project_id
   name         = "@.${local.zone_dns_name}"
   managed_zone = local.zone_name
-  type         = "CNAME"
+  type         = each.key
   ttl          = 300
 
-  rrdatas = ["ghs.googlehosted.com."]
+  rrdatas = each.value
 }
