41bcb5720fd1d11b4cde8d73bbcbdfa73e0f7fca
Christian Roggia <christian.roggia@gmail.com>
2020-05-13 00:23:24 +0200

    Transfer resources to new project

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -1,31 +1,31 @@
-# locals {
-#   zone_project_id = data.terraform_remote_state.root.outputs.project_id
-#   zone_dns_name   = data.terraform_remote_state.root.outputs.managed_zone_animeshon_com_dns_name
-#   zone_name       = data.terraform_remote_state.root.outputs.managed_zone_animeshon_com_name
-# }
+locals {
+  managed_zone_project_id = data.terraform_remote_state.root.outputs.project_id
+  managed_zone_dns_name   = data.terraform_remote_state.root.outputs.managed_zone_animeshon_com_dns_name
+  managed_zone_name       = data.terraform_remote_state.root.outputs.managed_zone_animeshon_com_name
+}
 
-# # Setup homepage preview subdomain managed by Google Cloud Run.
-# locals {
-#   dns_records = {
-#     "A" = [
-#       for rr in google_cloud_run_domain_mapping.homepage.status[0].resource_records :
-#       rr.rrdata if rr.type == "A"
-#     ]
-#     "AAAA" = [
-#       for rr in google_cloud_run_domain_mapping.homepage.status[0].resource_records :
-#       rr.rrdata if rr.type == "AAAA"
-#     ]
-#   }
-# }
+# Setup homepage preview subdomain managed by Google Cloud Run.
+locals {
+  dns_records = {
+    "A" = [
+      for rr in google_cloud_run_domain_mapping.homepage.status[0].resource_records :
+      rr.rrdata if rr.type == "A"
+    ]
+    "AAAA" = [
+      for rr in google_cloud_run_domain_mapping.homepage.status[0].resource_records :
+      rr.rrdata if rr.type == "AAAA"
+    ]
+  }
+}
 
-# resource "google_dns_record_set" "homepage" {
-#   for_each = local.dns_records
+resource "google_dns_record_set" "homepage" {
+  for_each = local.managed_zone_project_id
 
-#   project      = local.zone_project_id
-#   name         = "@.${local.zone_dns_name}"
-#   managed_zone = local.zone_name
-#   type         = each.key
-#   ttl          = 300
+  project      = local.zone_project_id
+  name         = "@.${local.managed_zone_dns_name}"
+  managed_zone = local.managed_zone_name
+  type         = each.key
+  ttl          = 300
 
-#   rrdatas = each.value
-# }
+  rrdatas = each.value
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -2,86 +2,9 @@ provider "google" {
   version = "~> 3.20"
 }
 
-# # NOTE: A new id is generated each time we switch to a new image tag.
-# resource "random_id" "homepage" {
-#   keepers = {
-#     image_tag = var.image_tag
-#   }
+locals {
+  project_id     = data.terraform_remote_state.general.outputs.project_id
+  project_number = data.terraform_remote_state.general.outputs.project_number
 
-#   byte_length = 8
-# }
-
-# resource "google_cloud_run_service" "homepage" {
-#   project  = data.terraform_remote_state.root.outputs.project_id
-#   location = "europe-west1"
-#   name     = "homepage-animeshon-com"
-
-#   template {
-#     metadata {
-#       annotations = {
-#         "autoscaling.knative.dev/maxScale" = "5"
-#         "run.googleapis.com/client-name"   = "cloud-console"
-#       }
-#       name = format("homepage-animeshon-com-%s", random_id.homepage.hex) 
-#     }
-
-#     spec {
-#       container_concurrency = 80
-#       service_account_name  = data.terraform_remote_state.root.outputs.compute_default_service_account_email
-
-#       containers {
-#         image = format("gcr.io/gcp-animeshon/homepage:%s", var.image_tag)
-
-#         env {
-#           name  = "HOST"
-#           value = "animeshon.com"
-#         }
-
-#         resources {
-#           limits = {
-#             cpu    = "1000m"
-#             memory = "256Mi"
-#           }
-#         }
-#       }
-#     }
-#   }
-
-#   traffic {
-#     percent         = 100
-#     latest_revision = true
-#   }
-# }
-
-# # Configure the domain name mapping for the instance to animeshon.com.
-# resource "google_cloud_run_domain_mapping" "homepage" {
-#   project  = google_cloud_run_service.homepage.project
-#   location = google_cloud_run_service.homepage.location
-#   name     = "animeshon.com"
-
-#   metadata {
-#     namespace = data.terraform_remote_state.root.outputs.project_id
-#   }
-
-#   spec {
-#     route_name = google_cloud_run_service.homepage.name
-#   }
-# }
-
-# # Allow everyone to access this instance from animeshon.com.
-# data "google_iam_policy" "noauth" {
-#   binding {
-#     role = "roles/run.invoker"
-#     members = [
-#       "allUsers",
-#     ]
-#   }
-# }
-
-# resource "google_cloud_run_service_iam_policy" "homepage" {
-#   project  = google_cloud_run_service.homepage.project
-#   location = google_cloud_run_service.homepage.location
-#   service  = google_cloud_run_service.homepage.name
-
-#   policy_data = data.google_iam_policy.noauth.policy_data
-# }
+  sa_compute_email = data.terraform_remote_state.general.outputs.google_compute_default_service_account_email
+}
