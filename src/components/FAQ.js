5cadf0473209b24cc625a277837e389f7f84e900
Luscha <matteo.roggia@gmail.com>
2020-07-25 17:39:43 +0200

    removed faq dynamic load

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -6,67 +6,59 @@ const FAQ = ({ accordionOnClick, strings, lang }) => (
     <div className="manga-feature faq-feature">
         <h2 className={lang === 'ja' ? 'italic' : ''}>{strings.headline}</h2>
         <div className="accordion">
-            {strings.questionTwo && (
-                <div
-                    className="question"
-                    data-collapsed={1}
-                    onClick={e => accordionOnClick(e)}
-                    onKeyDown={() => {}}
-                    role="button"
-                    tabIndex={0}
-                >
-                    <p data-collapsed={1}>{strings.questionOne}</p>
-                    <div data-collapsed={1} className="contents">
-                        <p>{strings.responseOne}</p>
-                    </div>
+            <div
+                className="question"
+                data-collapsed={1}
+                onClick={e => accordionOnClick(e)}
+                onKeyDown={() => {}}
+                role="button"
+                tabIndex={0}
+            >
+                <p data-collapsed={1}>{strings.questionOne}</p>
+                <div data-collapsed={1} className="contents">
+                    <p>{strings.responseOne}</p>
                 </div>
-            )}
-            {strings.questionTwo && (
-                <div
-                    className="question"
-                    data-collapsed={2}
-                    onClick={e => accordionOnClick(e)}
-                    onKeyDown={() => {}}
-                    role="button"
-                    tabIndex={0}
-                >
-                    <p data-collapsed={2}>{strings.questionTwo}</p>
-                    <div data-collapsed={2} className="contents">
-                        <p>{strings.responseTwo}</p>
-                    </div>
+            </div>
+            <div
+                className="question"
+                data-collapsed={2}
+                onClick={e => accordionOnClick(e)}
+                onKeyDown={() => {}}
+                role="button"
+                tabIndex={0}
+            >
+                <p data-collapsed={2}>{strings.questionTwo}</p>
+                <div data-collapsed={2} className="contents">
+                    <p>{strings.responseTwo}</p>
                 </div>
-            )}
-            {strings.questionThree && (
-                <div
-                    className="question"
-                    data-collapsed={3}
-                    onClick={e => accordionOnClick(e)}
-                    onKeyDown={() => {}}
-                    role="button"
-                    tabIndex={0}
-                >
-                    <p data-collapsed={3}>{strings.questionThree}</p>
-                    <div data-collapsed={3} className="contents">
-                        <p>{strings.responseThree}</p>
-                    </div>
+            </div>
+            <div
+                className="question"
+                data-collapsed={3}
+                onClick={e => accordionOnClick(e)}
+                onKeyDown={() => {}}
+                role="button"
+                tabIndex={0}
+            >
+                <p data-collapsed={3}>{strings.questionThree}</p>
+                <div data-collapsed={3} className="contents">
+                    <p>{strings.responseThree}</p>
                 </div>
-            )}
+            </div>
 
-            {strings.questionFour && (
-                <div
-                    className="question"
-                    data-collapsed={4}
-                    onClick={e => accordionOnClick(e)}
-                    onKeyDown={() => {}}
-                    role="button"
-                    tabIndex={0}
-                >
-                    <p data-collapsed={4}>{strings.questionFour}</p>
-                    <div data-collapsed={4} className="contents">
-                        <p>{strings.responseFour}</p>
-                    </div>
+            <div
+                className="question"
+                data-collapsed={4}
+                onClick={e => accordionOnClick(e)}
+                onKeyDown={() => {}}
+                role="button"
+                tabIndex={0}
+            >
+                <p data-collapsed={4}>{strings.questionFour}</p>
+                <div data-collapsed={4} className="contents">
+                    <p>{strings.responseFour}</p>
                 </div>
-            )}
+            </div>
         </div>
     </div>
 );
