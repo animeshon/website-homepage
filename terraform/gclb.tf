1c0ea2ad9ab59c7cb74ca69821b6f764804a36f1
Christian Roggia <christian.roggia@gmail.com>
2020-11-23 01:15:13 +0100

    Fix HTTP to HTTPS redirection

--- terraform/gclb.tf
+++ terraform/gclb.tf
@@ -5,9 +5,9 @@ locals {
 
 # Setup the backend service  with Cloud CDN enabled.
 resource "google_compute_backend_service" "homepage" {
-  name        = "animeshon-com--homepage"
-  enable_cdn  = true
-  protocol    = "HTTPS"
+  name       = "animeshon-com--homepage"
+  enable_cdn = true
+  protocol   = "HTTPS"
 
   backend {
     group = google_compute_region_network_endpoint_group.homepage.id
@@ -15,9 +15,9 @@ resource "google_compute_backend_service" "homepage" {
 }
 
 resource "google_compute_backend_service" "encyclopedia" {
-  name        = "animeshon-com--encyclopedia"
-  enable_cdn  = true
-  protocol    = "HTTPS"
+  name       = "animeshon-com--encyclopedia"
+  enable_cdn = true
+  protocol   = "HTTPS"
 
   backend {
     group = google_compute_region_network_endpoint_group.encyclopedia.id
@@ -39,7 +39,7 @@ resource "google_compute_region_network_endpoint_group" "encyclopedia" {
   name                  = "encyclopedia-neg"
   network_endpoint_type = "SERVERLESS"
   region                = "europe-west1"
-#   region                = local.run_service_encyclopedia_location
+  #   region                = local.run_service_encyclopedia_location
 
   cloud_run {
     service = local.run_service_encyclopedia_name
@@ -60,12 +60,6 @@ resource "google_compute_url_map" "animeshon_com_https" {
     name            = "default"
     default_service = google_compute_backend_service.homepage.id
 
-    route_rules {
-      url_redirect {
-        https_redirect = true
-      }
-    }
-
     path_rule {
       paths = ["/e/*", "/e"] # Encyclopedia URI - redirect all traffic to its Cloud Run instance.
       route_action {
@@ -80,11 +74,13 @@ resource "google_compute_url_map" "animeshon_com_https" {
 }
 
 resource "google_compute_url_map" "animeshon_com_http" {
-  name            = "animeshon-com-http"
-  default_service = google_compute_backend_service.homepage.id
+  name = "animeshon-com--http"
 
   default_url_redirect {
-    https_redirect = true
+    https_redirect         = true
+    redirect_response_code = "MOVED_PERMANENTLY_DEFAULT"
+
+    strip_query = false
   }
 }
 
